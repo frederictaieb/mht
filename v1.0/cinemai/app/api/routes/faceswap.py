1c1972a2b88cadf71dde64dabc71ecf833810a8d
fete <fete@MacBook-Pro-de-fete.local>
2026-02-14 08:58:41 +0100

    Ajout de la route pour supprimer les images. A faire : l'upload d'images

--- v1.0/cinemai/app/api/routes/faceswap.py
+++ v1.0/cinemai/app/api/routes/faceswap.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+import os
+from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 
 from app.core.config import settings
@@ -28,3 +29,39 @@ def faceswap_single(payload: FaceSwapPathsRequest):
         target_video_path=payload.target_video_path,
         output_name=payload.output_name,
     )
+
+@router.post("/upload")
+def upload_image():
+    return
+
+
+@router.delete("/delete")
+def delete_images():
+    deleted = 0
+    INPUT_DIR = settings.INPUT_DIR
+
+    if not os.path.exists(INPUT_DIR):
+        raise HTTPException(status_code=404, detail="Input directory not found")
+    
+    if not os.path.isdir(INPUT_DIR):
+        raise HTTPException(status_code=400, detail="INPUT_DIR n'est pas un dossier")
+
+    for filename in os.listdir(INPUT_DIR):
+        path = os.path.join(INPUT_DIR, filename)
+
+        try:
+            if os.path.isfile(path) or os.path.islink(path):
+                os.unlink(path)
+                deleted += 1
+            elif os.path.isdir(path):
+                shutil.rmtree(path)
+                deleted += 1
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"Erreur suppression: {str(e)}")
+
+    return {
+        "message": "input directory cleared",
+        "deleted_items": deleted
+    }
+    return
+
--- v1.0/cinemai/app/core/config.py
+++ v1.0/cinemai/app/core/config.py
@@ -6,8 +6,8 @@ class Settings:
     BASE_DIR: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # app/..
     INPUT_DIR: str = os.path.join(BASE_DIR, "data/input")
     OUTPUT_DIR: str = os.path.join(BASE_DIR, "data/output")
-    MODELS_DIR: str = os.path.join(BASE_DIR, "ai_models")
 
+    MODELS_DIR: str = os.path.join(BASE_DIR, "data/ai_models")
     SWAPPER_MODEL: str = os.path.join(MODELS_DIR, "inswapper_128.onnx")
 
     # InsightFace
